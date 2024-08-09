fb86ac7446712d4624ea0471f8a4fc1ebd0f3806
DisaroSkell <80709519+DisaroSkell@users.noreply.github.com>
2024-08-09 12:21:25 +0200

    fix: sorted type selectors options
    * fix: type selectors should be in text language and not poke language

--- src/app/_components/typesGuessSelectors/component.tsx
+++ src/app/_components/typesGuessSelectors/component.tsx
@@ -1,9 +1,10 @@
 'use client';
 
-
 import { useEffect } from "react";
+import { useTranslation } from "react-i18next";
 
 import { PokeType } from "@/src/types/pokeType.type";
+import { defaultLanguage } from "@/src/types/lang.type";
 
 import CustomSelect from "../customSelect/component";
 
@@ -22,18 +23,34 @@ export default function TypesGuessSelectors({
     onTypesChange
 }: TypesGuessSelectorsProps) {
     const dispatch = useAppDispatch();
-    const selectedLang = useAppSelector(state => state.lang.selectedLang);
+    const langs = useAppSelector(state => state.lang.langs);
     const pokeTypes = useAppSelector(selectPokeTypes);
+    
+    const { i18n } = useTranslation();
 
     useEffect(() => {
-        dispatch(fetchPokeTypes(selectedLang));
-    }, [dispatch, selectedLang]);
+        let foundLang = langs.find(l => l.id.toLowerCase() === i18n.language.toLowerCase());
+
+        if (!foundLang) foundLang = defaultLanguage;
+            
+        dispatch(fetchPokeTypes(foundLang));
+    }, [dispatch, langs, i18n]);
 
     function getPokeTypesAsOptions(pokeTypes: PokeType[]) {
         return [{value: '', label: ' '}, ...pokeTypes.map(type => {return {
             value: type.id,
             label: type.fullName
-        }})];
+        }}).sort((a, b) => {
+            if (a.label.match(/[^\p{L}]/gu) && !b.label.match(/[^\p{L}]/gu)) {
+                return 1;
+            }
+            
+            if (b.label.match(/[^\p{L}]/gu) && !a.label.match(/[^\p{L}]/gu)) {
+                return -1;
+            }
+
+            return a.label.localeCompare(b.label);
+        })];
     }
 
     function changeTypes(type1Value: string, type2Value: string) {
