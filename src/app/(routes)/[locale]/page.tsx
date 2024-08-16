12d5d7e5934d95af3a8520ec9c1bf8146dbab52a
DisaroSkell <80709519+DisaroSkell@users.noreply.github.com>
2024-08-16 10:30:46 +0200

    fix: deploy in output export should work properly
    * fix: root path should redirect to lang stored in cookie
    * refactor: split layout in 2

--- src/app/(routes)/[locale]/page.tsx
+++ src/app/(routes)/[locale]/page.tsx
@@ -1,13 +1,18 @@
 import Link from "next/link";
 
+import { i18nSupportedLanguages } from "@/i18nConfig";
+
 import initTranslations from "@/src/i18n";
 
-import LanguageSelectors from "@/src/app/_components/languageSelectors/component";
 import TranslationsProvider from "@/src/app/_components/providers/translationsProvider";
 
 import "./page.css";
 
 const i18nNamespaces = ["home", "common"];
+
+export async function generateStaticParams() {
+    return i18nSupportedLanguages.map((i18nLang) => ({locale: i18nLang}));
+}
 
 interface HomePageProps {
     params: { locale: string }
---
+++ src/app/(routes)/page.tsx
@@ -0,0 +1,5 @@
+import Redirector from '../_components/redirector/redirector';
+
+export default async function Page() {
+    return <Redirector path="/" />
+}
