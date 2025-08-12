33a5604be4938d01e0088aa20ba898de5e17e531
DisaroSkell <Hydralisk34980@gmail.com>
2025-08-12 15:20:39 +0200

    feat: placeholders for panels

--- src/app/_components/tutorialModal/component.tsx
+++ src/app/_components/tutorialModal/component.tsx
@@ -18,26 +18,78 @@ export default function TutorialModal() {
 
     const [isOpen, setIsOpen] = useState(true);
 
-    const disableDisplayTutorial = useCallback(() => {
+    const [currentPanel, setCurrentPanel] = useState(0);
+
+    const onCloseTutorial = useCallback(() => {
         dispatch(setDisplayTutorialSetting(false));
+        setIsOpen(false);
     }, [dispatch]);
 
-    const onSkipTutorial = useCallback(() => {
-        disableDisplayTutorial();
-        setIsOpen(false);
-    }, [disableDisplayTutorial]);
+    const prevPanel = () => setCurrentPanel(current =>
+        (current === 0 ? current : current - 1)
+    )
+
+    const nextPanel = () => setCurrentPanel(current =>
+        (current === panels.length - 1 ? current : current + 1)
+    )
 
     return isOpen && displayTutorialSetting && <div className='tutorialModalBg'>
         <div className='pokeCard tutorialModal'>
-            <h2>{t('common:welcome')}</h2>
-            <div className="pokeCard">
-                <LanguageSelectors />
+            <h1>{t('common:welcome')}</h1>
+            {panels[currentPanel]()}
+            <div className='navigateButtonsContainer'>
+                <CustomButton
+                    label='<'
+                    type={'secondary'}
+                    onClickCallback={prevPanel}
+                    disabled={currentPanel === 0}
+                />
+                {
+                    currentPanel === panels.length - 1 ?
+                    <CustomButton
+                        label={t('common:confirm')}
+                        type={'secondary'}
+                        onClickCallback={onCloseTutorial}
+                    /> :
+                    <CustomButton
+                        label='>'
+                        type={'secondary'}
+                        onClickCallback={nextPanel}
+                    />
+                }
             </div>
-            <CustomButton
-                label={t('common:confirm')}
-                type={'secondary'}
-                onClickCallback={onSkipTutorial}
-            />
         </div>
     </div>;
+}
+
+const panels = [
+    LangPanel,
+    QuizPresPanel,
+    SettingsPresPanel,
+    // EmptyPanel,
+]
+
+function LangPanel() {
+    return <>
+        <div className="pokeCard">
+            <LanguageSelectors />
+        </div>
+        <p>Don't forget to validate for it to take effect. Next press on the arrow at the bottom right.</p>
+    </>
+}
+
+function QuizPresPanel() {
+    return <>
+        Hello, here is an explaination of how quizzes work and presentation of their settings (for desktop AND mobile).
+        <div className="quizSettings">Desktop explain</div>
+        <div className="quizSettingsMobile">Mobile explain</div>
+    </>
+}
+
+function SettingsPresPanel() {
+    return <>And here we present the settings page.</>
+}
+
+function EmptyPanel() {
+    return <></>
 }
